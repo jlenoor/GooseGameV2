a24f3ca5d946e74a35250bbf921178f1ebfb86af
bernd_herssens <berndherssens@hotmail.com>
2024-05-06 14:55:29 +0200

    eerste aanzet gamestate

--- Ganzenbord.Business/Player/Player.cs
+++ Ganzenbord.Business/Player/Player.cs
@@ -1,4 +1,6 @@
-﻿namespace Ganzenbord.Business.Player
+﻿using Ganzenbord.Business.Services;
+
+namespace Ganzenbord.Business.Player
 {
     public class Player : IPlayer
     {
@@ -12,13 +14,40 @@
         public int CurrentPosition { get; set; }
         public int PreviousPosition { get; set; }
         public int RolledValue { get; set; }
+        public int NeedsToSkip { get; set; }
+        public bool IsStuck { get; private set; }
 
-
+        public void PlayerPlayATurn()
+        {
+            
+            if (NeedsToSkip < 0) 
+            {
+                NeedsToSkip--;
+            }
+            else if (IsStuck)
+            {
+                GameState.NextPlayer();  
+            }
+            else
+            {
+                //Dice.Roll(2);
+                Move(RolledValue, PreviousPosition);
+            }
+        }
         public void Move(int rolledDice, int oldPosition)
         {
             RolledValue = rolledDice;
             CurrentPosition = oldPosition + rolledDice;
         }
 
+        public void MoveThroughEvents(int destination)
+        {
+            CurrentPosition += destination;
+        }
+
+        public void SkipTurn(int wait)
+        {
+            NeedsToSkip += wait;
+        }
     }
 }
---
+++ Ganzenbord.Business/Square/Inn.cs
@@ -0,0 +1,17 @@
+﻿using Ganzenbord.Business.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ganzenbord.Business.Square
+{
+    internal class Inn : ISquare
+    {
+        public void HandlePlayer(IPlayer testPlayer)
+        {
+            testPlayer.SkipTurn(1);
+        }
+    }
+}
